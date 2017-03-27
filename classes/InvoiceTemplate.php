de9b8e5db0bf94b5fb0e6f1954e12d458cd3fbf5
Alexandr Kulishov <alkul93123@gmail.com>
2017-03-28 00:59:03 +0300

    Update InvoiceTemplate.php

--- classes/InvoiceTemplate.php
+++ classes/InvoiceTemplate.php
@@ -18,18 +18,6 @@ class InvoiceTemplate extends BaseTemplate {
 
     public function beforeRender($object)
     {
-        $this->totalPriceStr = $this->num2str($object->total_price);
-
-        $this->totalPriceStr = $this->num2str($object->total_price);
-        if ($object->organization->getFiles()->where(['=', 'type', 1])->one()) {
-            $this->pathStamp = stristr($object->organization->getFiles()
-                            ->where(['=', 'type', 1])->one()->location, '/uploads');
-        }
-        if ($object->organization->getFiles()->where(['=', 'type', 2])->one()) {
-            $this->pathSignature = stristr($object->organization->getFiles()
-                            ->where(['=', 'type', 2])->one()->location, '/uploads');
-        }
-
         $this->nds = $this->calculateNds($object);
         return $object;
     }
